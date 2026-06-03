3e9d0ff59a9f28b0ea1aee0aad708593a9b39012
Roshan <roshan.rai29904@gmail.com>
2026-06-04 04:04:46 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+
+SAMPLE_RATE = 22050
+DURATION = 4
+SAMPLE = SAMPLE_RATE  * DURATION
+N_MELS = 128
+
+CLASS_NAMES = [
+    "Air Conditioner", "Car Horn", "Children Playing", "Dog Bark", "Drilling", "Engine Idling", "Gun Shot", "Jackhammer", "Siren", "Street Music"
+]
+ 
